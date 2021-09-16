f09932a9445c24ccaf606a98c6a9025adefc741c
Nikita Babko <niki.babko@gmail.com>
2021-09-16 12:16:34 +0700

    Added styles to ProductScreen

--- client/src/js/screens/ProductScreen.js
+++ client/src/js/screens/ProductScreen.js
@@ -1,5 +1,6 @@
 import { getProduct } from '../api.js';
 import { parseRequestUrl } from '../utils.js';
+import Rating from '../components/Rating.js';
 
 const ProductScreen = {
   render: async () => {
@@ -10,7 +11,58 @@ const ProductScreen = {
       return `<div>${product.error}</div>`;
     }
     return `
-      <h1>${product.name}</h1>
+      <div class="content">
+        <div class="back-to-result">
+          <a href="/#/">Back to result</a>
+        </div>
+
+        <div class="details">
+          <div class="details-image">
+            <img src="${product.image}" alt="${product.name}" />
+          </div>
+
+          <div class="details-info">
+            <ul>
+              <li>
+                <h1>${product.name}</h1>
+              </li>
+              <li>
+                ${Rating.render({
+                  value: product.rating,
+                  text: `${product.numReviews} reviews`,
+                })}
+              </li>
+              <li>
+                Price <strong>${product.price}</strong>
+              </li>
+              <li>
+                Description:
+                <div>
+                  ${product.description}
+                </div>
+              </li>
+            </ul>
+          </div>
+
+          <div class="details-action">
+              <ul>
+                <li>
+                  Price $${product.price}
+                </li>
+                <li>
+                  Status ${
+                    product.countInStock > 0
+                      ? `<span class="success">In Stock</span>`
+                      : `<span class="error">Unavailable</span>`
+                  }
+                </li>
+                <li>
+                  <button id="add-button" class="fw primary">Add to cart</button>
+                </li>
+              </ul>
+          </div>
+        </div>
+      </div>
     `;
   },
 };
