379ecd4fc0c7df604db6b71e22402bfe77ca320f
Nikita Babko <niki.babko@gmail.com>
2021-10-03 22:18:06 +0700

    Added interface for token.

--- server/src/utils/interfaces.ts
+++ server/src/utils/interfaces.ts
@@ -1,8 +1,10 @@
 import { Document } from 'mongoose';
+import { Secret } from 'jsonwebtoken';
 
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
   isAdmin: boolean;
+  token?: Secret;
 }
